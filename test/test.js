d6e672df5a91d25b0a1cf3001f4e54fffe407e32
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-07-29 19:21:34 +0200

    test: add two simple test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,14 @@
 // Require Third-party Dependencies
 const avaTest = require("ava");
+const is = require("@slimio/is");
 
-avaTest("Husky passed Test", (assert) => {
-    assert.pass();
+// Require Internal Dependencies
+const prettyJSON = require("../");
+
+avaTest("prettyJSON must be a function", (assert) => {
+    assert.true(is.func(prettyJSON));
+});
+
+avaTest("prettyJSON of non-object must return undefined", (assert) => {
+    assert.is(prettyJSON("hello"), undefined);
 });
