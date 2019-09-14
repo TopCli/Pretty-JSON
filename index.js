7646110b46f2dfce842964e15b61fcf8f3f6f2f2
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-09-14 16:59:52 +0200

    fix: many stdout bugs

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// Require Node.js Dependencies
-const { EOL } = require("os");
-
 // Require Third-party Dependencies
 const { white, gray, bold } = require("kleur");
 const is = require("@slimio/is");
@@ -10,18 +7,29 @@ const is = require("@slimio/is");
 // Require Internal Dependencies
 const { maxKeyLength, primeColor } = require("./src/utils");
 
+// CONSTANTS
+const EOL = "\n";
+
 /**
  * @function logArray
  * @param {!Array<any>} arr
  * @param {number} [depth=1]
+ * @param {boolean} [disableEndLine=false]
  * @returns {void}
  */
-function logArray(arr, depth = 1) {
+function logArray(arr, depth = 1, disableEndLine = false) {
     const backStart = depth === 2 ? " " : "  ".repeat(depth - 1);
     const startSpace = depth === 1 ? " " : "  ".repeat(depth);
     const lenMax = arr.length - 1;
     const returnLine = arr.length > 10;
     const firstIsObject = arr.length > 0 && is.plainObject(arr[0]);
+    let forceNext = false;
+
+    if (arr.length === 0) {
+        process.stdout.write(`${gray("[]")}${disableEndLine ? "" : EOL}`);
+
+        return;
+    }
 
     process.stdout.write(`${gray("[")}${EOL}${firstIsObject ? "" : startSpace}`);
     for (let id = 0; id < arr.length; id++) {
@@ -31,21 +39,22 @@ function logArray(arr, depth = 1) {
             if (id !== 0) {
                 process.stdout.write(startSpace);
             }
-            logArray(value, depth + 1);
+            logArray(value, depth + 1, true);
+            forceNext = true;
             if (id !== lenMax) {
                 process.stdout.write(gray(", "));
                 process.stdout.write(EOL);
             }
         }
         else if (is.plainObject(value)) {
-            logObject(value, depth);
-            if (id !== lenMax) {
-                process.stdout.write(EOL);
-            }
+            logObject(value, depth, true);
+            forceNext = true;
         }
         else {
-            if (returnLine && id !== 0) {
+            // eslint-disable-next-line
+            if ((returnLine && id > 0) || forceNext) {
                 process.stdout.write(startSpace);
+                forceNext = false;
             }
 
             process.stdout.write(primeColor(value)(is.string(value) ? `'${value}'` : String(value)));
@@ -57,19 +66,26 @@ function logArray(arr, depth = 1) {
             }
         }
     }
-    process.stdout.write(`${EOL}${backStart}${gray("]")}`);
+    process.stdout.write(`${EOL}${backStart}${gray("]")}${disableEndLine ? "" : EOL}`);
 }
 
 /**
  * @function logObject
  * @param {!object} obj
  * @param {number} [depth=1]
+ * @param {boolean} [disableEndLine=false]
  * @returns {void}
  */
-function logObject(obj, depth = 1) {
+function logObject(obj, depth = 1, disableEndLine = false) {
     const betweenSpace = maxKeyLength(obj, 4);
     const startSpace = depth === 1 ? " " : "  ".repeat(depth);
     const entries = Object.entries(obj);
+
+    if (entries.length === 0) {
+        process.stdout.write(`${gray("{}")}${disableEndLine ? "" : EOL}`);
+
+        return;
+    }
 
     for (let id = 0; id < entries.length; id++) {
         const [key, value] = entries[id];
@@ -77,6 +93,9 @@ function logObject(obj, depth = 1) {
             continue;
         }
 
+        if (id === 0) {
+            process.stdout.write(EOL);
+        }
         process.stdout.write(bold(white(`${startSpace}${key}: ${" ".repeat(betweenSpace - key.length)}`)));
         if (is.object(value)) {
             (Array.isArray(value) ? logArray : logObject)(value, depth + 1);
@@ -84,7 +103,7 @@ function logObject(obj, depth = 1) {
         }
 
         process.stdout.write(primeColor(value)(is.string(value) ? `'${value}'` : String(value)));
-        if (id !== entries.length - 1) {
+        if (!disableEndLine) {
             process.stdout.write(EOL);
         }
     }
